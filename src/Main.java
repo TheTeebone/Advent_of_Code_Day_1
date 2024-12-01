d7852ce8c51b9d339a992968291fcd0d9e503d98
TheTeebone <t.seifert.tobias@aol.com>
2024-12-01 17:24:00 +0100

    initial push

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "input.txt";
+
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2= new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Zeile splitten
+                String[] parts = line.trim().split("\\s+");
+                if (parts.length == 2) {
+                    // Zahlen parsen und in die Listen einfügen
+                    list1.add(Integer.parseInt(parts[0]));
+                    list2.add(Integer.parseInt(parts[1]));
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Fehler beim Lesen der Datei: " + e.getMessage());
+        }
+
+        Collections.sort(list1);
+        Collections.sort(list2);
+        List<Integer> distance= new ArrayList<>();
+        Integer result = 0;
+
+        for(int i = 0 ; i < list1.size() ; i++) {
+            if(list1.get(i) <= list2.get(i)) {
+                distance.add(list2.get(i) - list1.get(i));
+            } else distance.add(list1.get(i) - list2.get(i));
+            result = result + distance.get(i);
+        }
+        System.out.println(result);
+    }
+}
